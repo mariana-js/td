97157997c51cd8d465593d6fd442003c985a86d7
Mariana Silva <109996561+mariana-js@users.noreply.github.com>
2024-12-10 22:19:15 -0300

    Update todo.service.ts

--- src/service/todo.service.ts
+++ src/service/todo.service.ts
@@ -17,7 +17,7 @@ export interface Task {
   providedIn: 'root'
 })
 export class TodoService {
-  private api = 'https://db-9gbqrnb5k-marianas-projects-0a5d45dd.vercel.app/taks';
+  private api = 'https://db-9gbqrnb5k-marianas-projects-0a5d45dd.vercel.app/tasks';
   private tasks: Task[] = [];
   private orderCriterion: string = 'semordem';
 
